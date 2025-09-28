09bdad4b6cbc67409d7041f959a0e3e01580c6f9
tvenk <tvenk@VENKATA_PRASATH>
2025-09-28 16:25:38 +0530

    AJUU

---
+++ CoreJavaProject/src/Day3/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package Day3.encapsulation;
+
+public class Encapsulation {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(101);
+		obj.setName("Dharshini");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
