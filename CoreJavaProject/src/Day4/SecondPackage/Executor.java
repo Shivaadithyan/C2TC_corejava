3cb2d4a0e5082683736bfdbfa72d2258800a0517
tvenk <tvenk@VENKATA_PRASATH>
2025-09-30 21:31:51 +0530

    AJUU

---
+++ CoreJavaProject/src/Day4/SecondPackage/Executor.java
@@ -0,0 +1,14 @@
+package Day4.SecondPackage;
+import Day4.FirstPackage.Base;
+public class Executor {
+	public static void main(String[] args) {
+		Base b1 = new Base();
+
+		
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+	}
+
+}
